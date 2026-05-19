592000308b286fc8af95b2cbf68ce7a37fa74696
KushalSwaroop64 <kushalswaroop64@gmail.com>
2026-05-19 11:38:03 +0530

    edited file

--- app/app.py
+++ app/app.py
@@ -34,7 +34,7 @@ def add_user():
                    (data["name"], data["email"]))
     conn.commit()
     conn.close()
-    return jsonify({"mmes": "User added"}), 201
+    return jsonify({"message": "User added"}), 201
 
 
 
